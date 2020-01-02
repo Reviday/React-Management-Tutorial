74575a8c09af0520b94ff692c220bcc971ae4c09
Seong-Kyu Yun <reviday@naver.com>
2020-01-02 22:14:53 +0900

    Commit Lecture 6
    Apply Material UI
    6강 - Material UI를 적용하여 고객 테이블 디자인하기 [React와 Node.js를 활용한 고객 관리 시스템 개발 강의]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import Customer from './componets/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customer = [
   {
@@ -32,25 +50,38 @@ const customer = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
-          customer.map(c => {
-            return (
-            <Customer
-              key={c.id} // map을 사용할 때는 key를 사용해야만 한다.
-              id={c.id}
-              image={c.image}
-              birthday={c.birthday}
-              gender={c.birthday}
-              job={c.job}
-            />
-            );
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableCell>번호</TableCell>            
+            <TableCell>이미지</TableCell>   
+            <TableCell>이름</TableCell>   
+            <TableCell>생년월일</TableCell>   
+            <TableCell>성별</TableCell>   
+            <TableCell>직업</TableCell>   
+          </TableHead>
+          <TableBody>
+            {
+              customer.map(c => {
+                return (
+                <Customer
+                  key={c.id} // map을 사용할 때는 key를 사용해야만 한다.
+                  id={c.id}
+                  image={c.image}
+                  birthday={c.birthday}
+                  gender={c.birthday}
+                  job={c.job}
+                />
+                );
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
