8a73cd12a229b0abee753028737fa6d7ec19609f
Seong-Kyu Yun <reviday@naver.com>
2020-01-03 16:21:42 +0900

    Commit Lecture 18
    Update UI/UX & Add Customer Search Module
    18강 - 필터(Filter) 함수를 이용한 고객(Customer) 검색 기능 구현하기 [React와 Node.js를 활용한 고객 관리 시스템 개발 강의]
    
    - Complete.

--- client/src/App.js
+++ client/src/App.js
@@ -125,7 +125,8 @@ class App extends Component {
     super(props);
     this.state = {
       customers: '',
-      completed: 0
+      completed: 0,
+      searchKeyword: ''
     }
     this.stateRefresh = this.stateRefresh.bind(this);
   }
@@ -133,7 +134,8 @@ class App extends Component {
   stateRefresh = () => {
     this.setState({
       customers: '',
-      completed: 0
+      completed: 0,
+      searchKeyword: ''
     })
     this.callApi()
       .then(res => this.setState({customers: res}))
@@ -158,7 +160,21 @@ class App extends Component {
     this.setState({ completed: completed >= 100 ? 0 : completed + 1})
   }
 
+  handleValueChange = (e) => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
+
   render() {
+    const fileteredComponents = (data) => {
+      data = data.filter((c) => {
+        return c.name.indexOf(this.state.searchKeyword) > -1;
+      });
+      return data.map((c) => {
+        return <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
+      });
+    }
     const { classes } = this.props; // props : 변경될 수 없는 데이터 명시
     const cellList = ["번호", "프로필 이미지", "이름", "생년월일", "성별", "직업", "설정"];
     return (
@@ -186,6 +202,9 @@ class App extends Component {
                   root: classes.inputRoot,
                   input: classes.inputInput,
                 }}
+                name="searchKeyword"
+                value={this.state.searchKeyword}
+                onChange={this.handleValueChange}
                 inputProps={{ 'aria-label': 'search' }}
               />
             </div>
@@ -203,21 +222,9 @@ class App extends Component {
             </TableHead>
             <TableBody>
               {
-                this.state.customers ? this.state.customers.map(c => {
-                  return (
-                  <Customer
-                    stateRefresh={this.stateRefresh}
-                    key={c.id} // map을 사용할 때는 key를 사용해야만 한다.
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                  />
-                  );
-                }) : 
-                <TableRow>
+                this.state.customers ? 
+                  fileteredComponents(this.state.customers) 
+                :<TableRow>
                   <TableCell colSpan="6" align="center">
                     <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
                   </TableCell>
